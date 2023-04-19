679a2d83a12289d1c4f00dec9b77b46725c816f9
phamtanhoang <88889770+phamtanhoang@users.noreply.github.com>
2023-04-19 19:20:46 +0700

    DONE test case & update database

--- oumarket.sql
+++ oumarket.sql
@@ -84,7 +84,9 @@ CREATE TABLE `customer` (
   `phonenumber` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_vi_0900_as_cs DEFAULT NULL,
   `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_vi_0900_as_cs DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `phonenumber_UNIQUE` (`phonenumber`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vi_0900_as_cs;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -94,7 +96,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES ('1','Lê Đạt','2023-04-04 00:00:00','Nam','01232458743','dat2@gmail.com'),('2','Hoàng','2002-03-02 00:00:00','Nam','01232356423','hoang@gmail.com'),('3','Dũ','2002-12-31 00:00:00','Nam','01234534122','du@gmail.com'),('3340f0a8-151d-4f2a-87f6-30e7dffc667a','Ngọc Trinh','2002-12-05 00:00:00','Nữ','0362400302','ngọctrinh@gmail.com'),('4','Hiếu','2002-04-07 00:00:00','Nam','01234335443','hieu@gmail.com'),('7f847397-fd34-406c-8a34-427d3fa17558','Ngọc Trinh','2002-12-05 00:00:00','Nữ','0362400302','ngọctrinh@gmail.com'),('a92ca5f7-b4e7-4371-b763-6087151adb89','Ngọc Trinh1','1970-01-01 00:00:00','Nữ','0362400233','ngoctrinh02@gmail.com'),('afdb4126-8889-44c2-b3c3-68b25bac00a3','Ngọc Trinh1','2002-12-06 00:00:00','Nữ','0362400233','ngoctrinh02@gmail.com'),('c71c85e1-95e3-448e-a1e4-a41c840b08c1','Ngọc Trinh','2002-12-05 00:00:00','Nữ','0362400302','ngọctrinh@gmail.com');
+INSERT INTO `customer` VALUES ('1','Lê Đạt','2023-04-04 00:00:00','Nam','01232458743','dat2@gmail.com'),('2','Hoàng','2002-03-02 00:00:00','Nam','01232356423','hoang@gmail.com'),('3','Dũ','2023-04-19 00:00:00','Nam','01234534122','du@gmail.com'),('4','Hiếu','2002-04-20 00:00:00','Nam','01234335443','hieu@gmail.com');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +157,7 @@ CREATE TABLE `promotion` (
 
 LOCK TABLES `promotion` WRITE;
 /*!40000 ALTER TABLE `promotion` DISABLE KEYS */;
-INSERT INTO `promotion` VALUES ('036827bd-c647-422e-a040-2bbb243ee8f1','2023-04-02','2023-04-20',3000,'d4a9da6b-5010-4e26-8e36-96177aa896bb'),('2528b482-c97a-4b8a-b1fe-46136d2c9eac','2023-04-18','2023-04-18',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('422c0078-ecc5-4c40-9455-091cc91e5212','2023-04-18','2023-04-18',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('4b0b7d7b-8255-44dc-9618-33e07d7f5271','2023-04-18','2023-04-18',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('83c501cf-fada-49bd-b146-e81afde45f86','2023-04-17','2023-04-17',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('9feef2a3-f3d7-49a4-80cc-4e2b1afdc7da','2023-04-18','2023-04-18',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('ca39272d-c99c-4f52-8ed0-8fa50b775651','2023-04-05','2023-04-20',1000,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d'),('f8663ab0-b410-455d-9d27-0813f0603779','2023-04-18','2023-04-18',3,'aa0649d8-a32e-45eb-b059-4f3a84484352');
+INSERT INTO `promotion` VALUES ('036827bd-c647-422e-a040-2bbb243ee8f1','2023-04-03','2023-04-20',3000,'d4a9da6b-5010-4e26-8e36-96177aa896bb'),('250a8ab6-b3a8-4f56-9e66-115f5f4ae1dc','2023-04-20','2023-04-22',10000,'e275c05c-0351-4f23-a804-ef40005dcaca'),('c0da98a7-eddf-4c43-8cc6-08d9586ac2ae','2023-04-11','2023-04-27',3000,'aa0649d8-a32e-45eb-b059-4f3a84484352'),('ca39272d-c99c-4f52-8ed0-8fa50b775651','2023-04-05','2023-04-20',1000,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d');
 /*!40000 ALTER TABLE `promotion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -171,7 +173,7 @@ CREATE TABLE `receipt` (
   `createddate` datetime NOT NULL,
   `temptotal` float DEFAULT NULL,
   `promotiontotal` float DEFAULT NULL,
-  `birthday` bit(1) DEFAULT b'0',
+  `birthday` float DEFAULT '0',
   `total` float NOT NULL,
   `staffID` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_vi_0900_as_cs NOT NULL,
   `customerID` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_vi_0900_as_cs DEFAULT NULL,
@@ -188,7 +190,7 @@ CREATE TABLE `receipt` (
 
 LOCK TABLES `receipt` WRITE;
 /*!40000 ALTER TABLE `receipt` DISABLE KEYS */;
-INSERT INTO `receipt` VALUES ('3a8e7e74-160e-49a6-81aa-67bcfa06a0f0','2023-04-16 00:00:00',35000,9000,_binary '\0',26000,'64acd540-7c61-4dad-9e8f-6efba1343652','1'),('4781a975-ed8b-48af-9c6e-dddb79b8679d','2023-04-15 00:00:00',32000,0,_binary '\0',28800,'64acd540-7c61-4dad-9e8f-6efba1343652','1'),('744e7cc1-ef8c-4a71-8b45-adde54884ab6','2023-04-15 00:00:00',65000,29000,_binary '\0',32400,'64acd540-7c61-4dad-9e8f-6efba1343652','1'),('7ddd2de8-4c73-454e-b09a-25b4a37b9886','2023-04-15 00:00:00',94600,33000,_binary '\0',61600,'64acd540-7c61-4dad-9e8f-6efba1343652','4'),('e5245606-5595-40cb-af33-722b9ef33739','2023-04-16 00:00:00',30000,20000,_binary '\0',10000,'64acd540-7c61-4dad-9e8f-6efba1343652',NULL);
+INSERT INTO `receipt` VALUES ('c32759c1-b7a6-430a-9b06-519beca37eee','2023-04-19 00:00:00',1290000,31000,0.1,1133100,'64acd540-7c61-4dad-9e8f-6efba1343652','3'),('e5245606-5595-40cb-af33-722b9ef33739','2023-04-16 00:00:00',30000,20000,0,10000,'64acd540-7c61-4dad-9e8f-6efba1343652',NULL);
 /*!40000 ALTER TABLE `receipt` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,7 +219,7 @@ CREATE TABLE `receipt_detail` (
 
 LOCK TABLES `receipt_detail` WRITE;
 /*!40000 ALTER TABLE `receipt_detail` DISABLE KEYS */;
-INSERT INTO `receipt_detail` VALUES (1,'aa0649d8-a32e-45eb-b059-4f3a84484352','4781a975-ed8b-48af-9c6e-dddb79b8679d'),(1,'aa0649d8-a32e-45eb-b059-4f3a84484352','744e7cc1-ef8c-4a71-8b45-adde54884ab6'),(1,'aa0649d8-a32e-45eb-b059-4f3a84484352','7ddd2de8-4c73-454e-b09a-25b4a37b9886'),(1,'aa0649d8-a32e-45eb-b059-4f3a84484352','e5245606-5595-40cb-af33-722b9ef33739'),(1,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d','3a8e7e74-160e-49a6-81aa-67bcfa06a0f0'),(1,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d','744e7cc1-ef8c-4a71-8b45-adde54884ab6'),(1,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d','7ddd2de8-4c73-454e-b09a-25b4a37b9886'),(1,'d4a9da6b-5010-4e26-8e36-96177aa896bb','744e7cc1-ef8c-4a71-8b45-adde54884ab6'),(1.2,'d4a9da6b-5010-4e26-8e36-96177aa896bb','7ddd2de8-4c73-454e-b09a-25b4a37b9886'),(1,'d4a9da6b-5010-4e26-8e36-96177aa896bb','e5245606-5595-40cb-af33-722b9ef33739'),(1,'e275c05c-0351-4f23-a804-ef40005dcaca','3a8e7e74-160e-49a6-81aa-67bcfa06a0f0'),(1,'e275c05c-0351-4f23-a804-ef40005dcaca','4781a975-ed8b-48af-9c6e-dddb79b8679d'),(1,'e275c05c-0351-4f23-a804-ef40005dcaca','744e7cc1-ef8c-4a71-8b45-adde54884ab6'),(2,'e275c05c-0351-4f23-a804-ef40005dcaca','7ddd2de8-4c73-454e-b09a-25b4a37b9886');
+INSERT INTO `receipt_detail` VALUES (1,'aa0649d8-a32e-45eb-b059-4f3a84484352','c32759c1-b7a6-430a-9b06-519beca37eee'),(1,'aa0649d8-a32e-45eb-b059-4f3a84484352','e5245606-5595-40cb-af33-722b9ef33739'),(1,'b1b8c7f2-39e3-4660-8d2d-f1f94fa4269d','c32759c1-b7a6-430a-9b06-519beca37eee'),(1,'d4a9da6b-5010-4e26-8e36-96177aa896bb','c32759c1-b7a6-430a-9b06-519beca37eee'),(1,'d4a9da6b-5010-4e26-8e36-96177aa896bb','e5245606-5595-40cb-af33-722b9ef33739'),(50,'e275c05c-0351-4f23-a804-ef40005dcaca','c32759c1-b7a6-430a-9b06-519beca37eee');
 /*!40000 ALTER TABLE `receipt_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,6 +245,8 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `phonenumber_UNIQUE` (`phonenumber`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_user_branch_idx` (`branchID`),
   CONSTRAINT `fk_user_branch` FOREIGN KEY (`branchID`) REFERENCES `branch` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vi_0900_as_cs;
@@ -254,7 +258,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('2','Phạm Tấn Hoàng',NULL,NULL,'',NULL,'Admin','hoang','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','',NULL),('64acd540-7c61-4dad-9e8f-6efba1343652','Lê Quang Đạt','371 Nguyễn Kiệm','2023-04-06 00:00:00','Nam','01232','staff','dat1','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','304a012e-fdd4-44b2-b791-36c3ff5526d5','@@@'),('75b5743e-308f-461e-ad12-e36d46a3d586','Nguyễn Văn A','123 ABC St.','1970-01-01 00:00:00','Nam','0909123456','staff','nguyenvana','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvana@gmail.com'),('e8e64e90-a200-4651-806d-afeb41c41f13','Nguyễn Văn Bảo','456 XYZ St.','1970-01-01 00:00:00','Nam','0987654321','staff','nguyenvanb','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvanb@gmail.com'),('ff3c6de1-2396-4b7f-98c1-20e49f04a1e9','Nguyễn Văn D','123 ABC St.','1970-01-01 00:00:00','Nam','0909123423','staff','nguyenvand','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvand@gmail.com');
+INSERT INTO `user` VALUES ('2','Phạm Tấn Hoàng',NULL,NULL,'',NULL,'Admin','hoang','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','',NULL),('64acd540-7c61-4dad-9e8f-6efba1343652','Lê Quang Đạt','371 Nguyễn Kiệm','2023-04-06 00:00:00','Nam','0923564875','staff','dat1','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','304a012e-fdd4-44b2-b791-36c3ff5526d5','dat@gmail.com'),('98dffcc7-40f0-4057-be31-6d1f06654f78','Nguyễn Văn A','123 ABC St.','2002-12-06 00:00:00','Nam','0909123456','staff','nguyenvana','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvana@gmail.com'),('e8e64e90-a200-4651-806d-afeb41c41f13','Nguyễn Văn Bảo','456 XYZ St.','1970-01-01 00:00:00','Nam','0987654321','staff','nguyenvanb','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvanb@gmail.com'),('ff3c6de1-2396-4b7f-98c1-20e49f04a1e9','Nguyễn Văn D','123 ABC St.','1970-01-01 00:00:00','Nam','0909123423','staff','nguyenvand','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=','71c471a5-ce70-45bc-8b48-e3fbf5e78070','nguyenvand@gmail.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -267,4 +271,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-19 14:04:56
+-- Dump completed on 2023-04-19 19:18:56
